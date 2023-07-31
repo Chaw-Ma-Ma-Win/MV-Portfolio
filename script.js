fb32d794f5dfa072191cbcc13bd2b8cb4bcd8217
Chaw Ma Ma Win <108109152+Chaw-Ma-Ma-Win@users.noreply.github.com>
2023-07-31 18:53:39 +0630

    Change some script

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const cross = document.getElementById('cross');
 const mobileMenu = document.querySelector('.mobile-menu');
-const humburger = document.querySelector('.humburger');
-humburger.addEventListener('click', () => {
-  mobileMenu.style.display = 'flex';
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', () => {
+  mobileMenu.style.left = '0';
 });
 cross.addEventListener('click', () => {
-  mobileMenu.style.display = 'none';
+  mobileMenu.style.left = '-100%';
 });
+
+document.querySelectorAll('.link').forEach((n) => n.addEventListener('click', () => {
+  mobileMenu.style.left = '-100%';
+}));
